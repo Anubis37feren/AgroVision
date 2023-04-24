c7032e09c2eba82fd8b14d4d4245cb624e8ab94a
medoza <anonemehoureynatouadama@gmail.com>
2023-04-24 21:07:14 +0100

    Initial commit

---
+++ app/src/main/java/com/perfectcorp/brift/login.java
@@ -0,0 +1,16 @@
+package com.perfectcorp.brift;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.perfectcorp.brift.R;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/perfectcorp/brift/prelogin.java
@@ -0,0 +1,16 @@
+package com.perfectcorp.brift;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.perfectcorp.brift.R;
+
+public class prelogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prelogin);
+    }
+}
